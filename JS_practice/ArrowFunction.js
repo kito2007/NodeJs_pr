a29e55e1a6c3d8dfa845b374ebeb8661df1d6556
kito2007 <kito200742@gmail.com>
2023-04-15 17:25:36 +0900

    개체, 속성 및 메서드 작업 파트 완

--- JS_practice/ArrowFunction.js
+++ JS_practice/ArrowFunction.js
@@ -13,4 +13,14 @@ function summarizeUser(username, userAge, userHasHobbies){
     );
 }
 
+// 화살표 함수 예시
+// 입력 받는 파라미터가 하나일 때 괄호 생략가능
+const add = () => 1+2;
+const addOne = a => a+1;
+const addTwo = (a,b) => a+b;
+
+
+console.log(add());
+console.log(addOne(3));
+console.log(addTwo(3,2));
 console.log(summarizeUser(name, age, hasHobbies));
---
+++ JS_practice/object+structure+method.js
@@ -0,0 +1,21 @@
+const person = {
+    name: 'Max',
+    age: 29,
+    greet: () => {
+        console.log('Hi, I am ' + this.name);
+    },// 화살표 함수 사용시, this는 전역변수를 참조하기 때문에 'Hi, I am undefined'가 출력된다.
+    greet2: function() {
+        console.log('Hi, I am ' + this.name);
+    },
+    greet3() {
+        console.log('Hi, this is greet3 function, my name is ' + this.name);
+    }
+}
+
+person.greet(); // name은 undefined
+person.greet2(); // 정상적으로 name 변수에 저장된 Max 출력
+
+// 전역변수를 참조할 때는 화살표 함수 안에서 this를 사용해도 되지만
+// 객체 멤버를 사용할 때는 기본 함수형태나 함수 형태 없이 함수 본문만 선언한 형태로 구현한다.
+
+person.greet3();
